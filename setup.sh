2561d43b6f466a20f10f7846bf005dc4cbe8fb76
Wafi Hasan <80825555+wafi981@users.noreply.github.com>
2023-06-27 15:48:09 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -41,6 +41,12 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 # For ARM64
 [ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-arm64
 chmod +x ./kind
+
+# Helm
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
 sudo mv ./kind /usr/local/bin/kind
 
 
