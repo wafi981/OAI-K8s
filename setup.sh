36cff49dd59018817c256112b1fbb81b752170e6
Wafi Hasan <80825555+wafi981@users.noreply.github.com>
2023-06-27 13:18:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,8 +32,16 @@ docker compose version
 # x86-64
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+#Installing kind
+
+# For AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+# For ARM64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-arm64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
 
 
 
